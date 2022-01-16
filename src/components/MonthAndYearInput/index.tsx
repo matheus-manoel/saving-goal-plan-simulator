890590f2bde189b2d44ec4460eeb739676c5bfa2
Matheus <matheuscmanoel@gmail.com>
2022-01-16 12:42:37 -0300

    Add logic for date manipulation in input

--- src/components/MonthAndYearInput/index.tsx
+++ src/components/MonthAndYearInput/index.tsx
@@ -1,11 +1,16 @@
+import dayjs from 'dayjs';
+import localeData from 'dayjs/plugin/localeData';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+dayjs.extend(localeData);
 
 interface MonthAndYearInputProps {
   label?: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const MonthAndYearInputWrapper = styled.div`
+const OuterWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
@@ -13,14 +18,63 @@ const MonthAndYearInputWrapper = styled.div`
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const MonthAndYearInputTitle = styled.label``;
 
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const InnerWrapper = styled.div`
+  display: flex;
+`;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const MonthAndYearWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const MonthText = styled.span``;
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const YearText = styled.span``;
+
 export default function MonthAndYearInput(
   props: MonthAndYearInputProps
 ): JSX.Element {
   const { label } = props;
+  const [date, setDate] = useState(dayjs().add(1, 'year'));
+  const [shouldDisableLeftArrow, setShouldDisableLeftArrow] = useState(false);
+
+  const onRightArrowButtonClick = () => {
+    setDate(date.add(1, 'month'));
+  };
+
+  const onLeftArrowButtonClick = () => {
+    setDate(date.subtract(1, 'month'));
+  };
+
+  useEffect(() => {
+    const today = dayjs();
+    if (date.month() === today.month() && date.year() === today.year()) {
+      setShouldDisableLeftArrow(true);
+    } else {
+      setShouldDisableLeftArrow(false);
+    }
+  }, [date]);
+
   return (
-    <MonthAndYearInputWrapper>
+    <OuterWrapper>
       {label && <MonthAndYearInputTitle>{label}</MonthAndYearInputTitle>}
-      <input></input>
-    </MonthAndYearInputWrapper>
+      <InnerWrapper>
+        <button
+          onClick={onLeftArrowButtonClick}
+          disabled={shouldDisableLeftArrow}
+        >
+          {'<'}
+        </button>
+        <MonthAndYearWrapper>
+          <MonthText>{dayjs.months()[date.month()]}</MonthText>
+          <YearText>{date.year()}</YearText>
+        </MonthAndYearWrapper>
+        <button onClick={onRightArrowButtonClick}>{'>'}</button>
+      </InnerWrapper>
+    </OuterWrapper>
   );
 }
